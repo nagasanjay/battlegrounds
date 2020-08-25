eda2e096224bebea18653daa20155c0f70271260
nagasanjay <nagasajayvijayan@gmail.com>
2020-08-25 16:33:01 +0530

    update

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+	
+	'googleAuth' : {
+		'clientID':'981408776457-vium6s7e4rf8q665gu55dcq4nucf171e.apps.googleusercontent.com',
+        'clientSecret':'CSk4arG59V4PfqU7RdYwYLVd',
+		'callbackURL': 'http://localhost:3000/auth/google/callback'
+	}
+}
+
